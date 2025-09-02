376c6ded4fa17a1fbfaa13be215fba16806e0681
Sharan Jhangiani <jhangiani.sharan@gmail.com>
2025-09-02 15:23:21 -0700

    updates from wayback machine

---
+++ contact.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SoundToxins.org</title>
+<meta name="description" content="SoundToxins website (SoundToxins.org) - The Goal of the SoundToxins project is to provide sufficient warning of HAB and "Vibrio" events to enable early or selective harvesting of shellfish in Puget Sound." />
+<meta name="keywords" content="harmful,algal,blooms,puget,sound,washington,vibrio,pseudo,nitzschia,toxin" />
+<link href="soundtoxins.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table align="center"><tr><td>
+<div style="width:900px;"><img src="soundtox_banner.jpg" /></div>
+
+<div class="topnav"><table width="100%"><tr><td width="90%"><a class="topnav" href="index.php">Home</a> | <a class="topnav" href="participants.php">Participants</a> | <a class="topnav" href="meetings.php">Meetings</a> | <a class="topnav" href="outreach.php">Outreach</a> | <a class="topnav" href="links.php">Links</a> | <a class="topnav" href="contact.php">Contact</a></td><td align="right"><a class="topnav" href="private/">Log in</a></td></tr></table>
+</div>
+
+<!--<div style="width:900px; background-image:url(nav_bkgrd.jpg); background-repeat:repeat-x; color:#FFFFFF; padding-left:10px; padding-right:10px;"><table width="100%"><tr><td width="90%"><a class="topnav" href="index.php">Home</a> | <a class="topnav" href="participants.php">Participants</a> | <a class="topnav" href="meetings.php">Meetings</a> | <a class="topnav" href="outreach.php">Outreach</a> | <a class="topnav" href="links.php">Links</a> | <a class="topnav" href="contact.php">Contact</a></td><td align="right"><a class="topnav" href="private/">Log in</a></td></tr></table></div>-->
+
+<div class="bodytxt" align="center">
+
+<p class="bigtitle">SoundToxins Contact Info</p>
+
+<p><b>Mailing Address:</b><br />
+Northwest Fisheries Science Center<br />
+2725 Montlake Boulevard East<br />
+Seattle, WA 98112</p>
+
+<table width="275px"><tr valign="top"><td>
+<p><strong>Program Inquiries:</strong></p>
+<p>Vera Trainer<br /></p>
+<p>Rohinee Paranjpye</p>
+<p>&nbsp;</p>
+</td><td><p><a href="programemail.php">Send an email</a></p></td></tr>
+
+<tr valign="top"><td><p><strong>Website Inquiries:</strong></p>
+<p>Sheryl Day</p></td><td><p><a href="websiteemail.php">Send an email</a></p></td></tr>
+</table>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="200px" align="center"><tr><td>
+<p><a href="http://www.noaa.gov/privacy.html" target="_blank">Privacy Policy</a> | <a href="http://www.noaa.gov/disclaimer.html" target="_blank">Disclaimer</a></p>
+</td></tr></table>
+
+</div></td></tr></table>
+</body>
+</html>
---
+++ meetings.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SoundToxins.org</title>
+<meta name="description" content="SoundToxins website (SoundToxins.org) - The Goal of the SoundToxins project is to provide sufficient warning of HAB and "Vibrio" events to enable early or selective harvesting of shellfish in Puget Sound." />
+<meta name="keywords" content="harmful,algal,blooms,puget,sound,washington,vibrio,pseudo,nitzschia,toxin" />
+<link href="soundtoxins.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<table align="center"><tr><td>
+<div style="width:900px;"><img src="soundtox_banner.jpg" /></div>
+
+<div class="topnav"><table width="100%"><tr><td width="90%"><a class="topnav" href="index.php">Home</a> | <a class="topnav" href="participants.php">Participants</a> | <a class="topnav" href="meetings.php">Meetings</a> | <a class="topnav" href="outreach.php">Outreach</a> | <a class="topnav" href="links.php">Links</a> | <a class="topnav" href="contact.php">Contact</a></td><td align="right"><a class="topnav" href="private/">Log in</a></td></tr></table>
+</div>
+
+<!--<div style="width:900px; background-image:url(nav_bkgrd.jpg); background-repeat:repeat-x; color:#FFFFFF; padding-left:10px; padding-right:10px;"><table width="100%"><tr><td width="90%"><a class="topnav" href="index.php">Home</a> | <a class="topnav" href="participants.php">Participants</a> | <a class="topnav" href="meetings.php">Meetings</a> | <a class="topnav" href="outreach.php">Outreach</a> | <a class="topnav" href="links.php">Links</a> | <a class="topnav" href="contact.php">Contact</a></td><td align="right"><a class="topnav" href="private/">Log in</a></td></tr></table></div>-->
+
+<div class="bodytxt" align="center">
+
+<p class="bigtitle">Meetings</p>
+
+<p><strong>March 25, 2009</strong> - SoundToxins Workshop (Northwest Fisheries Science Center, Seattle, WA)<br />
+Training and refresher workshop for all SoundToxins participants.<br />
+Conducted by: Vera Trainer, Brian Bill, Keri Baugh, and Rita Horner.</p>
+<p><img src="images/group_pic.jpg" alt="SoundToxins workshop participants" /></p>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<table width="200px" align="center"><tr><td>
+<p><a href="http://www.noaa.gov/privacy.html" target="_blank">Privacy Policy</a> | <a href="http://www.noaa.gov/disclaimer.html" target="_blank">Disclaimer</a></p>
+</td></tr></table>
+
+
+</div></td></tr></table>
+</body>
+</html>
