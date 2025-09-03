f827a7bf5070f0cfc453856cfced16e7db07fc55
Sharan Jhangiani <jhangiani.sharan@gmail.com>
2025-09-02 20:17:54 -0700

    more updates from wayback archives

--- participants.php
+++ participants.php
@@ -1,45 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>SoundToxins.org</title>
-<meta name="description" content="SoundToxins website (SoundToxins.org) - The Goal of the SoundToxins project is to provide sufficient warning of HAB and "Vibrio" events to enable early or selective harvesting of shellfish in Puget Sound." />
-<meta name="keywords" content="harmful,algal,blooms,puget,sound,washington,vibrio,pseudo,nitzschia,toxin" />
-<link href="soundtoxins.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
-<table align="center"><tr><td>
-<div style="width:900px;"><img src="soundtox_banner.jpg" /></div>
-
-<div class="topnav"><table width="100%"><tr><td width="90%"><a class="topnav" href="index.php">Home</a> | <a class="topnav" href="participants.php">Participants</a> | <a class="topnav" href="meetings.php">Meetings</a> | <a class="topnav" href="outreach.php">Outreach</a> | <a class="topnav" href="links.php">Links</a> | <a class="topnav" href="contact.php">Contact</a></td><td align="right"><a class="topnav" href="private/">Log in</a></td></tr></table>
-</div>
-
-<!--<div style="width:900px; background-image:url(nav_bkgrd.jpg); background-repeat:repeat-x; color:#FFFFFF; padding-left:10px; padding-right:10px;"><table width="100%"><tr><td width="90%"><a class="topnav" href="index.php">Home</a> | <a class="topnav" href="participants.php">Participants</a> | <a class="topnav" href="meetings.php">Meetings</a> | <a class="topnav" href="outreach.php">Outreach</a> | <a class="topnav" href="links.php">Links</a> | <a class="topnav" href="contact.php">Contact</a></td><td align="right"><a class="topnav" href="private/">Log in</a></td></tr></table></div>-->
-
-<div class="bodytxt" align="center">
-
-<p class="bigtitle">SoundToxins Partners and Participants</p>
-<p>We thank all of the SoundToxins participants, without whose sampling efforts, the project could not exist:</p>
-<table width="700px" cellpadding="7px" cellspacing="0px" border="1px"><tr><td><p><strong>Agency</strong></p></td><td><p><strong>Participant</strong></p></td><td><p><strong>Website</strong></p></tr>
-
-<tr valign="middle"><td><p align="center"><img src="images/wsg_logo.jpg" align="middle" alt="Washington Sea Grant logo" />&nbsp;Washington Sea Grant</p></td><td><p>Teri King</p></td><td><p><a href="http://www.wsg.washington.edu/" target="_blank">http://www.wsg.washington.edu</a></p></td></tr>
-<tr valign="middle"><td><p><img src="images/jamestownsklallam_logo.jpg" align="middle" alt="Jamestown S'Klallam tribe logo" />&nbsp;Jamestown S’Klallam tribe</p></td><td><p>Lohna O’Rourke<br />Chris Whitehead</p></td><td><p><a href="http://www.jamestowntribe.org/" target="_blank">http://www.jamestowntribe.org</a></p></td></tr>
-<tr valign="middle"><td><p><img src="images/coastseafoods_logo.jpg" align="middle" alt="Coast Seafoods Co. logo" />&nbsp;Coast Seafoods Co.</p></td><td><p>Jon Zellmer<br />Sarah Whitten</p></td><td><p><a href="http://www.coastseafoods.com/" target="_blank">http://www.coastseafoods.com</a></p></td></tr>
-<tr valign="middle"><td><p align="center"><img src="images/penncove_logo.jpg" align="middle" alt="Penn Cove Shellfish, LLC logo" />&nbsp;Penn Cove Mussels LLC.</p></td><td><p>Tim Jones</p></td><td><p><a href="http://www.penncoveshellfish.com/" target="_blank">http://www.penncoveshellfish.com</a></p></td></tr>
-<tr valign="middle"><td><p>Vashon Island resident</p></td><td><p>Karlista Rickerson</p></td><td>&nbsp;</td></tr>
-<tr valign="middle"><td><p>Taylor Shellfish</p></td><td><p>Kelly Benardis<br />Jared Hammond</p></td><td><p><a href="http://www.taylorshellfishfarms.com/" target="_blank">http://www.taylorshellfishfarms.com</a></p></td></tr>
-<tr valign="middle"><td><p><img src="images/ptmsc_logo.jpg" align="left" alt="Port Townsend Marine Science Center logo" /><br />&nbsp;Port Townsend<br />&nbsp;Marine Science Center</p></td><td><p>Jean Wallat<br />Linda Dacon<br />Dan Darrow<br />Soozie Darrow<br />Karen Delorenzo<br />Pam Gray<br />Chris MacLachlan<br />Stephanie Oliviera<br />Cheqa Rodgers<br />Laura Rosenberg<br />Louise Walczak<br />Richard Williams<br />Brenda Banner</p></td><td><p><a href="http://www.ptmsc.org/" target="_blank">http://www.ptmsc.org</a></p></td></tr>
-<tr valign="middle"><td><p><img src="images/evergreen_college_logo.jpg" align="middle" alt="Evergreen State College logo" />&nbsp;Evergreen State College</p></td><td><p>Gerardo Chin-Leo</p></td><td><p><a href="http://www.evergreen.edu/" target="_blank">http://www.evergreen.edu/</a></p></td></tr>
-</table>
-
-<p>&nbsp;</p>
-<p>&nbsp;</p>
-<table width="200px" align="center"><tr><td>
-<p><a href="http://www.noaa.gov/privacy.html" target="_blank">Privacy Policy</a> | <a href="http://www.noaa.gov/disclaimer.html" target="_blank">Disclaimer</a></p>
-</td></tr></table>
-
-
-</div></td></tr></table>
-</body>
-</html>
+<?php
+// Participants listing is now organized under Partners pages
+header('Location: /partners-educators.html', true, 301);
+echo '<!doctype html><meta http-equiv="refresh" content="0; url=/partners-educators.html">';
+exit;
+?>
